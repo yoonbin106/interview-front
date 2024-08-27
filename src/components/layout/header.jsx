065b67f61a6fc414176f081e76ebdf842c7f92e6
ChooInCheol <gg9465@naver.com>
2024-08-27 19:04:26 +0900

    feat:설문조사 -> 적성탐색으로 변경 설문조사 gpt결과 가져올떄 기다리는동안 스피너추가
    날짜:2024-08-27 오후 7시

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -224,7 +224,7 @@ const Header = observer(() => {
                   </div>
                   <div className={styles.subMenus}>
                     <a href="/search" className={styles.subMenu}>상세 검색</a>
-                    <a href="/survey" className={styles.subMenu}>설문조사</a>
+                    <a href="/survey" className={styles.subMenu}>적성 탐색</a>
                   </div>
                   <div className={styles.subMenus}>
                     <a href="/resume/resumeList" className={styles.subMenu}>이력서</a>
